f79a2990b49a070c88c80df2ccae3d8870c7a179
kkang123 <85389685+kkang123@users.noreply.github.com>
2024-01-22 20:35:13 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -0,0 +1,14 @@
+def solution(num1, num2):
+    # answer = 0
+    # return answer
+
+    # if num1 == num2:
+    #     return 1
+    # else:
+    #     return -1
+    
+    # if num1 == num2:
+    #     return 1
+    # return -1
+
+    return 1 if num1 == num2 else -1 #사~ 연산자
