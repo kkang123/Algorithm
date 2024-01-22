a2a033cd2404e79e3f53e94940cbbae28b779f91
kkang123 <85389685+kkang123@users.noreply.github.com>
2024-01-22 20:29:55 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -0,0 +1,13 @@
+function solution(num1, num2) {
+    // var answer = 0;
+    // return answer;
+    return ~~(num1 / num2); //~~정수 표현
+}
+
+
+// function solution(num1, num2) {
+//   return parseInt(num1 / num2);
+// }
+
+
+// const solution = (num1, num2) => parseInt(num1 / num2)
