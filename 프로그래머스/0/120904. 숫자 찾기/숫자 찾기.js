e20790659d439536ad1accaf7c5242218a2f60b5
지헌 <85389685+kkang123@users.noreply.github.com>
2024-03-14 09:04:53 -0700

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,13 @@
+function solution(num, k) {
+    
+    return num.toString().indexOf(k) + 1 || -1
+    
+//     if(num.toString().indexOf(k) === -1){
+//         return -1
+//     } else{
+//         return num.toString().indexOf(k) + 1
+//     }
+//     return num
+// }
+
+}
