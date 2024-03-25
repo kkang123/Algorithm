a06007f4ed6153d2c26e1bc9fc64bbbf88dac644
지헌 <85389685+kkang123@users.noreply.github.com>
2024-03-24 19:57:34 -0700

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,19 @@
+function solution(num_list) {
+    total = [];
+    even = [];
+    odd = [];
+    
+    for(let i=0; i< num_list.length; i++){
+        if(num_list[i] % 2 === 0){
+         even.push(i);   
+        } else{
+            odd.push(i)
+        }
+    }
+    
+    
+    total[0] = even.length;
+    total[1] = odd.length;
+    
+    return total
+}
