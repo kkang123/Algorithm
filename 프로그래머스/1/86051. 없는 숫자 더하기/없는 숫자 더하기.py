4f7d0f2a28acfae061e9e104fd422ba2fda379d9
지헌 <85389685+kkang123@users.noreply.github.com>
2024-04-04 11:40:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -6,3 +6,16 @@ def solution(numbers):
     
     
     return res
+
+# 이렇게 풀어도 되는가? 다른 방법이 마땅히 떠오르지 않음..
+
+# 1.
+# return 45 - sum(numbers)
+
+# 2. not in문 사용하기
+# def solution(numbers):
+#     answer=0
+#     for i in range(1,10):
+#         if i not in numbers:
+#             answer += i
+#     return answer
