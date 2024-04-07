e67d2f30f558e5a29db4dd3057998fc5f54497c0
지헌 <85389685+kkang123@users.noreply.github.com>
2024-04-07 15:28:38 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,5 +1,9 @@
-def solution(n, k):
-    if n>=10:
-        k -= n//10
+# def solution(n, k):
+#     if n>=10:
+#         k -= n//10
 
-    return 12000 * n + k* 2000
+#     return 12000 * n + k* 2000
+
+
+def solution(n, k):
+    return 12000 * n + 2000 * (k - n // 10)
