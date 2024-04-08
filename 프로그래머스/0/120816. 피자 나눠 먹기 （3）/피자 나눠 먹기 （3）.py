eff0e2ff0701b7cc111dad1660c4470df0c84949
지헌 <85389685+kkang123@users.noreply.github.com>
2024-04-08 18:16:00 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,10 +1,12 @@
-# import math
+import math
 
-# def solution(slice, n):
-#     return math.ceil(n / slice)
+def solution(slice, n):
+    return math.ceil(n / slice)
 
 
 # 피자를 2~10 사이에 원하는 조각 수로 잘라줌(slice)
 
-def solution(slice, n):
-    return ((n-1) // slice) + 1
+# def solution(slice, n):
+#     return ((n-1) // slice) + 1
+
+# 다른 사람의 풀이 수식 사용 코드인데 n-1하는 것부터 어떤 접근식인지 모르겠음
