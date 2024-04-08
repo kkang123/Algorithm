7612dbc665ce7449ed03ebdfe93d77c4a82abac0
지헌 <85389685+kkang123@users.noreply.github.com>
2024-04-08 18:15:08 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,7 +1,10 @@
-import math
+# import math
 
-def solution(slice, n):
-    return math.ceil(n / slice)
+# def solution(slice, n):
+#     return math.ceil(n / slice)
 
 
 # 피자를 2~10 사이에 원하는 조각 수로 잘라줌(slice)
+
+def solution(slice, n):
+    return ((n-1) // slice) + 1
