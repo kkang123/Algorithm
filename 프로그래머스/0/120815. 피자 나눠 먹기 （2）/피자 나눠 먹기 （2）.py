8e03bfbd57d0dff53a5cef13bb88ab57fc9e9b27
지헌 <85389685+kkang123@users.noreply.github.com>
2024-04-08 18:03:50 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,5 +1,16 @@
-def solution(n):
-    for i in range(1, n+1):
-        if (6 * i) % n == 0:
-            return i
+# def solution(n):
+#     for i in range(1, n+1):
+#         if (6 * i) % n == 0:
+#             return i
         
+def solution(n):
+    i=1
+    while(1):
+        if (6*i)%n==0:
+            return i
+        i+=1
+        
+# while문이 1이기 때문에 True
+# if문은 거짓이기 때문에 return을 반환 안하는 구조
+# 그래서 아래 i+= 1이 실행 이 과정을 if문이 True 일 때까지 반복
+    
