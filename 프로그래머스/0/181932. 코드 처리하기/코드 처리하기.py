a4a4663c9a8bb101bec598f33306bcdae4bc612e
지헌 <85389685+kkang123@users.noreply.github.com>
2024-09-05 23:41:35 +0900

    [level 0] Title: 코드 처리하기, Time: 11.48 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,12 @@
+def solution(code):
+    res = ''
+    mode = 0
+    
+    for i in range(len(code)):
+        if code[i] == '1':
+            mode ^= 1
+        else:
+            if i % 2 == mode:
+                res += code[i]
+                
+    return res if res else 'EMPTY'
