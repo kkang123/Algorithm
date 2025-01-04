b3e04e04f97e7b1b120c4587d9f00da72e331593
지헌 <85389685+kkang123@users.noreply.github.com>
2025-01-04 20:57:45 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 109.77 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -11,15 +11,16 @@ def solution(arr):
             q.append(arr[i])
             
     return list(q)
+# rueturn q 가 아닌 이유는 deque는 다른 자료형이기 때문에 오류 발생
 
 # 스택
 
-def solution(arr):
-    stack = [arr[0]]
+# def solution(arr):
+#     stack = [arr[0]]
 
-    for i in range(1, len(arr)):
-        if arr[i] != arr[i-1]:
-            stack.append(arr[i])
+#     for i in range(1, len(arr)):
+#         if arr[i] != arr[i-1]:
+#             stack.append(arr[i])
         
     
-    return stack
+#     return stack
