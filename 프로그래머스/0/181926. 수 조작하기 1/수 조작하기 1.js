4de30c111c848c7e6146f2f9e5a312a6ba4500ab
지헌 <85389685+kkang123@users.noreply.github.com>
2025-01-11 20:46:32 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.33 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -18,3 +18,67 @@ function solution(n, control) {
     
     return n;
 }
+
+// forEach
+
+// function solution(n, control) {
+//     let list = control.split('');
+
+//     list.forEach(char => {
+//         if(char === 'w') {
+//             n += 1;
+//         } else if (char === 's') {
+//             n -= 1;
+//         } else if (char === 'd') {
+//             n += 10;
+//         } else {
+//             n -= 10;
+//         }
+//     });
+
+//     return n;
+// }
+
+// reduce
+
+// function solution(n, control) {
+//     return control.split('').reduce((acc, char) => {
+//         switch (char) {
+//             case 'w':
+//                 return acc + 1;
+//             case 's':
+//                 return acc - 1;
+//             case 'd':
+//                 return acc + 10;
+//             case 'a':
+//                 return acc - 10;
+//             default:
+//                 return acc;
+//         }
+//     }, n);
+// }
+
+// 기본
+
+// function solution(n, control) {
+//     for (let i = 0; i < control.length; i++) {
+//         switch (control[i]) {
+//             case 'w':
+//                 n += 1;
+//                 break;
+//             case 's':
+//                 n -= 1;
+//                 break;
+//             case 'd':
+//                 n += 10;
+//                 break;
+//             case 'a':
+//                 n -= 10;
+//                 break;
+//         }
+//     }
+
+//     return n;
+// }
+
+
