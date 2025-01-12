7586575fb114ff9f0cef2ae6a882fc0e7ba6c3a3
지헌 <85389685+kkang123@users.noreply.github.com>
2025-01-13 07:29:52 +0900

    [level 0] Title: 배열 만들기 2, Time: 75.71 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/README.md
+++ 프로그래머스/0/181921. 배열 만들기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 2 - 181921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921#) 
 
 ### 성능 요약
 
-메모리: 52.4 MB, 시간: 78.41 ms
+메모리: 52.5 MB, 시간: 75.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 13일 07:26:46
+2025년 01월 13일 07:29:50
 
 ### 문제 설명
 
--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -7,9 +7,35 @@ function solution(l, r) {
         }
     }
     
-    if(answer.length === 0){
-            return [-1];
-        }
-    
-    return answer;
+    return answer.length ? answer : [-1];
 }
+
+
+// // 이진수 문제
+
+// // 숫자 5로 현혹시켰지만 사실 이건 이진수 문제임.
+// function* gen50() {
+//     let i = 1;
+
+//     while(true) {
+//         yield Number(Number(i).toString(2)) * 5;
+//         i++;
+//     }
+// }
+// function solution(l, r) {
+//     const n = gen50();
+//     let a = 0;
+//     const arr = [];
+
+//     while(a < l) { a = n.next().value; }
+//     while(a <= r) { arr.push(a); a = n.next().value; }
+
+//     return arr.length ? arr : [-1];
+// }
+
+// // 3
+
+// function solution(l, r) {
+//     const result = Array.from({length:r-l+1}, (_,i)=>i+l).filter(n=>!/[^05]/.test(n));
+//     return result.length ? result : [-1];
+// }
