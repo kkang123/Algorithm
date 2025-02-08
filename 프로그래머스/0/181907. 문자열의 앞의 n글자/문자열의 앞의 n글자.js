c42c5222f76b19f9decba29ac5fec01ffb20bc25
지헌 <85389685+kkang123@users.noreply.github.com>
2025-02-08 19:34:39 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.js
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.js
@@ -7,3 +7,14 @@ function solution(my_string, n) {
     
     return res;
 }
+
+
+// function solution(my_string, n) {
+//     return my_string.slice(0, n);
+// }
+
+// function solution(my_string, n) {
+//     return my_string.substring(0, n);
+// }
+
+
