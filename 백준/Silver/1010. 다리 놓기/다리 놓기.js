4f17c22e59669f1283808732c85ce687a7f032f8
지헌 <85389685+kkang123@users.noreply.github.com>
2025-04-23 21:46:15 +0900

    [Silver V] Title: 다리 놓기, Time: 116 ms, Memory: 10628 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.js
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.js
@@ -1,25 +1,22 @@
-
 const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n");
 
-function factorial(n) {
-  let result = 1n;
-  for (let i = 2n; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-}
+function combination(n, r) {
+  if (r > n) [r, n] = [n, r]; 
 
-function combination(r, n) {
-  r = BigInt(r);
-  n = BigInt(n);
-  return factorial(n) / (factorial(r) * factorial(n - r));
+  let result = 1;
+  for (let i = 1; i <= r; i++) {
+    result *= n - i + 1;
+    result /= i;
+  }
+
+  return Math.round(result);
 }
 
 for (let i = 1; i < input.length; i++) {
   const [a, b] = input[i].split(" ").map(Number);
-  console.log(combination(a, b).toString());
+  console.log(combination(a, b));
 }
