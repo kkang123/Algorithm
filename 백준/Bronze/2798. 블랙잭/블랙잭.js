b92d6662bffb466a4006e4ca4e86b8048f6b505a
지헌 <85389685+kkang123@users.noreply.github.com>
2025-05-08 02:11:05 +0900

    [Bronze II] Title: 블랙잭, Time: 148 ms, Memory: 9680 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -0,0 +1,21 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input.shift().split(" ").map(Number);
+const cards = input[0].split(" ").map(Number);
+
+let maxSum = 0;
+
+for (let i = 0; i < N; i++) {
+  for (let j = i + 1; j < N; j++) {
+    for (let k = j + 1; k < N; k++) {
+      const sum = cards[i] + cards[j] + cards[k];
+      if (sum <= M && sum > maxSum) maxSum = sum;
+    }
+  }
+}
+
+console.log(maxSum);
