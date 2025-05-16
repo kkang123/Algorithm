a6c3a09108176286e8290b9600f92f03681a8415
지헌 <85389685+kkang123@users.noreply.github.com>
2025-05-16 21:28:45 +0900

    [level 0] Title: 카운트 다운, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,9 +1,20 @@
-function solution(start_num, end_num) {
+// function solution(start_num, end_num) {
+//     let answer = [];
+    
+//     for(let i = end_num; i <= start_num; i++){
+//         answer.push(i);
+//     }
+    
+//     return answer.reverse();
+// }
+
+function solution(start, end) {
     let answer = [];
-    
-    for(let i = end_num; i <= start_num; i++){
-        answer.push(i);
+
+    for(let i = start; i>=end; i--){
+        answer.push(i)
     }
-    
-    return answer.reverse();
+
+    return answer;
 }
+
