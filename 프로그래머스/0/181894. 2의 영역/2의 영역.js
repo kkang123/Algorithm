8f12499eba995de35b463c194918952f666cef15
지헌 <85389685+kkang123@users.noreply.github.com>
2025-05-22 19:10:42 +0900

    [level 0] Title: 2의 영역, Time: 33.96 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,8 +1,34 @@
+// function solution(arr) {
+//     let first = arr.indexOf(2);
+//     let last = arr.lastIndexOf(2);
+    
+//     if(first === -1) return [-1]
+    
+//     return arr.slice(first, last + 1);
+// }
+
+
 function solution(arr) {
-    let first = arr.indexOf(2);
-    let last = arr.lastIndexOf(2);
+  let first = -1;
+  let last = -1;
+
+
+  for(let i = 0; i<arr.length; i++){
+      if(arr[i] === 2){
+          if(first === -1){
+              first = i
+          }
+          last = i
+      }
+  }
     
-    if(first === -1) return [-1]
+    if (first === -1) return [-1]
     
-    return arr.slice(first, last + 1);
+    let res = []
+    
+    for(let i=first; i<last+1; i++){
+        res.push(arr[i])
+    }
+    
+    return res
 }
