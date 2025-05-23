fd2b05c9823d3aaad519aa2f1afcf7dd7d340e00
지헌 <85389685+kkang123@users.noreply.github.com>
2025-05-23 22:38:29 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -0,0 +1,13 @@
+function solution(num_list, n) {
+    return num_list.slice(n-1)
+}
+
+
+// function solution(num_list, n) {
+//     let answer = [];
+    
+//     for(let i = n-1; i < num_list.length; i++){
+//         answer.push(num_list[i])
+//     }
+//     return answer;
+// }
