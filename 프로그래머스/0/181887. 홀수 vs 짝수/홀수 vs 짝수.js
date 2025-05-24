18d96ca83ca7ddfdb001db42716b073800980a6f
지헌 <85389685+kkang123@users.noreply.github.com>
2025-05-24 22:54:45 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,14 +1,24 @@
 function solution(num_list) {
-    let odd = 0; 
-    let even = 0;
+    const oddSum = num_list.filter((_, idx) => idx % 2 === 0).reduce((acc, cur) => acc + cur, 0);
     
-    for(let i = 0; i < num_list.length; i++){
-        if(i % 2 === 0){
-            odd += num_list[i];
-        } else {
-            even += num_list[i];
-        }
-    }
+    const evenSum = num_list.filter((_, idx) => idx % 2 === 1).reduce((acc, cur) => acc + cur, 0)
     
-    return Math.max(odd, even)
+    return Math.max(oddSum, evenSum);
 }
+
+
+
+// function solution(num_list) {
+//     let odd = 0; 
+//     let even = 0;
+    
+//     for(let i = 0; i < num_list.length; i++){
+//         if(i % 2 === 0){
+//             odd += num_list[i];
+//         } else {
+//             even += num_list[i];
+//         }
+//     }
+    
+//     return Math.max(odd, even)
+// }
