ffee2730489ea3e6511a4e2cd44123b710277559
지헌 <85389685+kkang123@users.noreply.github.com>
2025-05-29 15:07:32 +0900

    [level 0] Title: 1로 만들기, Time: 0.27 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    let count = 0;
+    
+    num_list.forEach((num) =>{
+        while (num != 1){
+            if(num % 2 === 0){
+                num /= 2;
+            } else if(num % 2 === 1) {
+                num = (num - 1) / 2
+            }
+            count++
+        } 
+    });
+    return count
+}
