197c42a6a417067870650ce5092fc40242b619a3
지헌 <85389685+kkang123@users.noreply.github.com>
2025-05-29 16:43:40 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,11 @@
+function solution(n_str) {
+    return Number(n_str).toString();
+}
+
+// function solution(n_str) {
+//   let arr = n_str.split('');
+//   while (arr[0] === '0') {
+//     arr.shift();
+//   }
+//   return arr.join('');
+// }
