8856fbafc2ba774b9dfa7a5aa81a169fe078c263
지헌 <85389685+kkang123@users.noreply.github.com>
2025-06-15 18:52:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,12 +1,30 @@
+// function solution(cards1, cards2, goal) {
+//     for(let word of goal){
+//         if(cards1[0] === word){
+//             cards1.shift()
+//         } else if (cards2[0] === word){
+//             cards2.shift()
+//         } else{
+//             return "No"
+//         } 
+//     }
+//     return "Yes"
+// }
+
+
 function solution(cards1, cards2, goal) {
+    let i = 0;
+    let j = 0;
+    
+    
     for(let word of goal){
-        if(cards1[0] === word){
-            cards1.shift()
-        } else if (cards2[0] === word){
-            cards2.shift()
-        } else{
+        if(i < cards1.length && cards1[i] === word){
+            i++
+        } else if (j < cards2.length && cards2[j] === word){
+            j++
+        } else {
             return "No"
-        } 
+        }
     }
     return "Yes"
 }
